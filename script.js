458d13e3337ec97d1f02c7ecc39e494f9231c4f2
Victor Matias <fvictor1705@gmail.com>
2022-05-26 18:22:19 -0300

    Resolve requisito 9.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function createPixels(quantity) { // cria o quadro de pixels
-
   const wayToBoard = document.getElementById('pixel-board');
 
   for (let i = 0; i < quantity; i += 1) {
@@ -15,8 +14,8 @@ createPixels(25);
 //-----------------------------------------------------------------------------
 
 function setBlackFirst() { // seta a cor black com a classe selected
-  const wayToBlack = document.getElementById('black');
-  wayToBlack.classList.add('selected');
+	const wayToBlack = document.getElementById('black');
+	wayToBlack.classList.add('selected');
 }
 
 setBlackFirst();
@@ -24,40 +23,38 @@ setBlackFirst();
 //-----------------------------------------------------------------------------
 
 function colorSelected() { // seleciona uma cor de cada vez
+	const wayToColor = document.querySelectorAll('.color');
 
-  const wayToColor = document.querySelectorAll('.color');
-  
-  for (let colors of wayToColor) {
-    colors.addEventListener('click', function returnTarget(event) {
-      const wayToBlack = document.getElementById('black');
-      const wayToRed = document.getElementById('red');
-      const wayToBlue = document.getElementById('blue');
-      const wayToGreen = document.getElementById('green');
-    
-      if (event.target.innerText === 'red') {
-        event.target.classList.add('selected');
-        wayToBlack.classList.remove('selected');
-        wayToBlue.classList.remove('selected');
-        wayToGreen.classList.remove('selected');
-      } else if (event.target.innerText === 'blue') {
-        event.target.classList.add('selected');
-        wayToBlack.classList.remove('selected');
-        wayToRed.classList.remove('selected');
-        wayToGreen.classList.remove('selected');
-      } else if (event.target.innerText === 'green') {
-        event.target.classList.add('selected');
-        wayToBlack.classList.remove('selected');
-        wayToRed.classList.remove('selected');
-        wayToBlue.classList.remove('selected');
-      } else {
-        wayToBlack.classList.add('selected');
-        wayToGreen.classList.remove('selected');
-        wayToRed.classList.remove('selected');
-        wayToBlue.classList.remove('selected');
-      }
-    
-    })
-  }
+	for (let colors of wayToColor) {
+		colors.addEventListener('click', function returnTarget(event) {
+			const wayToBlack = document.getElementById('black');
+			const wayToRed = document.getElementById('red');
+			const wayToBlue = document.getElementById('blue');
+			const wayToGreen = document.getElementById('green');
+
+			if (event.target.innerText === 'red') {
+				event.target.classList.add('selected');
+				wayToBlack.classList.remove('selected');
+				wayToBlue.classList.remove('selected');
+				wayToGreen.classList.remove('selected');
+			} else if (event.target.innerText === 'blue') {
+				event.target.classList.add('selected');
+				wayToBlack.classList.remove('selected');
+				wayToRed.classList.remove('selected');
+				wayToGreen.classList.remove('selected');
+			} else if (event.target.innerText === 'green') {
+				event.target.classList.add('selected');
+				wayToBlack.classList.remove('selected');
+				wayToRed.classList.remove('selected');
+				wayToBlue.classList.remove('selected');
+			} else {
+				wayToBlack.classList.add('selected');
+				wayToGreen.classList.remove('selected');
+				wayToRed.classList.remove('selected');
+				wayToBlue.classList.remove('selected');
+			}
+		})
+	}
 }
 
 colorSelected();
@@ -65,17 +62,29 @@ colorSelected();
 //-----------------------------------------------------------------------------
 
 function fillPixel() { // preenche os pixels com a cor selecionada
- const wayToDivs = document.querySelectorAll('.pixel');
+  const wayToDivs = document.querySelectorAll('.pixel');
 
- function color(event) {
-  event.target.style.backgroundColor = getComputedStyle(document.querySelector('.selected')).backgroundColor;
- }
+  function color(event) {
+    event.target.style.backgroundColor = getComputedStyle(document.querySelector('.selected')).backgroundColor;
+	}
 
- for (let div of wayToDivs) {
-   div.addEventListener('click', color)
- }
-  
-  
+  for (let div of wayToDivs) {
+    div.addEventListener('click', color)
+  }
 }
 fillPixel();
 
+//-----------------------------------------------------------------------------
+
+function clearBoard() {
+  const wayToButton = document.querySelector('#clear-board');
+  const wayToDivs = document.querySelectorAll('.pixel');
+
+  wayToButton.addEventListener('click', function test() {
+    for (let div of wayToDivs) {
+			div.style.backgroundColor = 'rgb(255, 255, 255)';
+		}
+	});
+}
+
+clearBoard();

2065b7d725586cb76c52b69baf965efe388e3360
Victor Matias <fvictor1705@gmail.com>
2022-05-26 16:28:13 -0300

    Resolve o requisito 8 e troca o valor das cores pelo formato RGB.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const wayToBoard = document.getElementById('pixel-board');
+function createPixels(quantity) { // cria o quadro de pixels
 
-function createPixels(quantity) {
+  const wayToBoard = document.getElementById('pixel-board');
+
   for (let i = 0; i < quantity; i += 1) {
     const createDiv = document.createElement('div');
     wayToBoard.appendChild(createDiv);
@@ -22,38 +23,59 @@ setBlackFirst();
 
 //-----------------------------------------------------------------------------
 
-const wayToColor = document.querySelectorAll('.color');
-const wayToBlack = document.getElementById('black');
-const wayToRed = document.getElementById('red');
-const wayToBlue = document.getElementById('blue');
-const wayToGreen = document.getElementById('green');
+function colorSelected() { // seleciona uma cor de cada vez
 
-for (let colors of wayToColor) {
-  colors.addEventListener('click', function returnTarget(event) {
-    console.log(event.target.innerText);
-    if (event.target.innerText === 'red') {
-      event.target.classList.add('selected');
-      wayToBlack.classList.remove('selected');
-      wayToBlue.classList.remove('selected');
-      wayToGreen.classList.remove('selected');
-    } else if (event.target.innerText === 'blue') {
-      event.target.classList.add('selected');
-      wayToBlack.classList.remove('selected');
-      wayToRed.classList.remove('selected');
-      wayToGreen.classList.remove('selected');
-    } else if (event.target.innerText === 'green') {
-      event.target.classList.add('selected');
-      wayToBlack.classList.remove('selected');
-      wayToRed.classList.remove('selected');
-      wayToBlue.classList.remove('selected');
-    } else {
-      wayToBlack.classList.add('selected');
-      wayToGreen.classList.remove('selected');
-      wayToRed.classList.remove('selected');
-      wayToBlue.classList.remove('selected');
-    }
-  })
+  const wayToColor = document.querySelectorAll('.color');
+  
+  for (let colors of wayToColor) {
+    colors.addEventListener('click', function returnTarget(event) {
+      const wayToBlack = document.getElementById('black');
+      const wayToRed = document.getElementById('red');
+      const wayToBlue = document.getElementById('blue');
+      const wayToGreen = document.getElementById('green');
+    
+      if (event.target.innerText === 'red') {
+        event.target.classList.add('selected');
+        wayToBlack.classList.remove('selected');
+        wayToBlue.classList.remove('selected');
+        wayToGreen.classList.remove('selected');
+      } else if (event.target.innerText === 'blue') {
+        event.target.classList.add('selected');
+        wayToBlack.classList.remove('selected');
+        wayToRed.classList.remove('selected');
+        wayToGreen.classList.remove('selected');
+      } else if (event.target.innerText === 'green') {
+        event.target.classList.add('selected');
+        wayToBlack.classList.remove('selected');
+        wayToRed.classList.remove('selected');
+        wayToBlue.classList.remove('selected');
+      } else {
+        wayToBlack.classList.add('selected');
+        wayToGreen.classList.remove('selected');
+        wayToRed.classList.remove('selected');
+        wayToBlue.classList.remove('selected');
+      }
+    
+    })
+  }
 }
 
+colorSelected();
 
+//-----------------------------------------------------------------------------
+
+function fillPixel() { // preenche os pixels com a cor selecionada
+ const wayToDivs = document.querySelectorAll('.pixel');
+
+ function color(event) {
+  event.target.style.backgroundColor = getComputedStyle(document.querySelector('.selected')).backgroundColor;
+ }
+
+ for (let div of wayToDivs) {
+   div.addEventListener('click', color)
+ }
+  
+  
+}
+fillPixel();
 
